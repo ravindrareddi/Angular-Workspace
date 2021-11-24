7f21a7e87f76f1d89ebce0749776b1b1d6c79607
rsatya1978 <satya.dasari.com>
2021-11-25 01:57:34 +0530

    commit1

--- ANGCMS/src/app/loginsucess/loginsucess.component.ts
+++ ANGCMS/src/app/loginsucess/loginsucess.component.ts
@@ -10,7 +10,7 @@ import * as $ from 'jquery';
   styleUrls: ['./loginsucess.component.css']
 })
 export class LoginsucessComponent implements OnInit {
-  strEmail:string | undefined
+
   title = 'Dynamic form';
   private incp:number = 1;
   private dynahtml:(any);
@@ -20,7 +20,6 @@ export class LoginsucessComponent implements OnInit {
   ngOnInit(){
     console.log(sessionStorage.getItem('email'));
     console.log(sessionStorage.getItem('password'));
-    this.strEmail = sessionStorage.getItem('email')?.toString();
     console.log('insidesucess');
   //   console.log(this.router.getCurrentNavigation().extras.state.example); 
     //this.interactionService.interactionmessage$.subscribe(message =>{console.log('-----new---')});
@@ -37,7 +36,7 @@ export class LoginsucessComponent implements OnInit {
       this.incp = $('#dyelement').children('tr').length+1;
       console.log($('#dyelement').children('tr').length);
       //alert(this.incp);
-      this.dynahtml ='<tr id="tr'+this.incp+'" style="height:50px;"><td style="width:20%; padding:20px;  vertical-align: top;">'+$("#labelname").val()+'</td><td style="padding:20px;  vertical-align:top;"><input type="labelname" placeholder="Enter '+$("#labelname").val()+'" name="'+$("#labelname").val()+'" id="'+$("#labelname").val()+'"></td></tr>';
+      this.dynahtml ='<tr id="tr'+this.incp+'" style="height:50px;"><td style="width:20%; padding:20px;  vertical-align: center;font-size: 17px;">'+$("#labelname").val()+'</td><td style="padding:20px;  vertical-align:center;font-size: 17px;"><input style="width: 50%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;" type="text" ngModel placeholder="Enter '+$("#labelname").val()+'" name="'+$("#labelname").val()+'" id="'+$("#labelname").val()+'"></td></tr>';
       console.log(this.dynahtml);
      
       
@@ -51,7 +50,7 @@ export class LoginsucessComponent implements OnInit {
       this.incp = $('#dyelement').children('tr').length+1;
       console.log($('#dyelement').children('tr').length);
       //alert(this.incp);
-      this.dynahtml ='<tr id="tr'+this.incp+'" style="height:50px;"><td style="width:20%; padding:20px;  vertical-align: top;">'+$("#labelname").val()+'</td><td style="padding:20px;  vertical-align:top;"><textarea name="'+$("#labelname").val()+'" id="'+$("#labelname").val()+'" rows="2" cols="30"></textarea></td></tr>';
+      this.dynahtml ='<tr id="tr'+this.incp+'" style="height:50px;"><td style="width:20%; padding:20px;  vertical-align: center;font-size: 17px;">'+$("#labelname").val()+'</td><td style="padding:20px;  vertical-align:center;font-size: 17px;"><textarea style="width: 50%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;" ngModel name="'+$("#labelname").val()+'" id="'+$("#labelname").val()+'" rows="2" cols="30"></textarea></td></tr>';
       console.log(this.dynahtml);
      
       
