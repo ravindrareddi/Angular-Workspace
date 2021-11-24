20c6d684ab95848fb8e5856fc420b68a88c9df45
rsatya1978 <satya.dasari.com>
2021-11-25 03:18:49 +0530

    c2

--- ANGCMS/src/app/login/login.component.ts
+++ ANGCMS/src/app/login/login.component.ts
@@ -11,7 +11,7 @@ import { Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  title = 'login form1';
+  title = 'login form';
   @Output() 
   stringOutput: EventEmitter<String> = new EventEmitter<String>();
   constructor(private router: Router) {
